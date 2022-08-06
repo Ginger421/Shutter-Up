c9095ff5703a20078e8f8209df5173a95ce5e048
Awollert <adamcwollert@gmail.com>
2022-08-06 10:38:13 -0600

    Server and Handlebars 2nd commit

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//handlebars here
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
