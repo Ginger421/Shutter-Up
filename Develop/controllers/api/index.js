45e0a55c2aecd594471b6f3e1c8fa81e753507ca
LisaTLe <lisa.le1127@gmail.com>
2022-08-08 19:15:45 -0600

    update routes links

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-const userRoutes = require("./photoRoutes");
+const userRoutes = require("./userRoutes");
+const photoRoutes = require("./photoRoutes");
 
 router.use("/users", userRoutes);
+router.use("/photos", photoRoutes);
 
 module.exports = router;
