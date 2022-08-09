1c7d150fb5643814f4dd148aa60b3d941e46a982
Awollert <adamcwollert@gmail.com>
2022-08-08 20:35:24 -0600

    Package commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const session = require("express-session");
+const exphbs = require("express-handlebars");
+const path = require("path");
 const routes = require("./controllers");
-
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 //handlebars here
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use(routes);
 app.use(session(sess));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
+app.use(express.static(path.join(__dirname, "public")));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
