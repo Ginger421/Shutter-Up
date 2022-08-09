ba610b894838d8a74bae90ee3fa35c2a5a990c73
LisaTLe <lisa.le1127@gmail.com>
2022-08-08 18:53:06 -0600

    update seeds

--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -1,11 +1,23 @@
 const sequelize = require("../config/connection");
+const { User, Photo } = require("../models");
 
-const seedPhoto = require("./photoData");
+const userData = require("./userData.json");
+const photoData = require("./photoData.json");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedPhoto();
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const photo of photoData) {
+    await Photo.create({
+      ...photo,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
