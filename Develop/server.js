a17a0273f90fe81f56fcd06aa64e1183c2dc8e0a
Awollert <adamcwollert@gmail.com>
2022-08-08 21:24:59 -0600

    Update

--- Develop/server.js
+++ Develop/server.js
@@ -4,11 +4,7 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const routes = require("./controllers");
-<<<<<<< HEAD
-=======
-const exphbs = require("express-handlebars");
 
->>>>>>> 36a618ca97c09265071b49a32d07d05cca6214d0
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
 
@@ -20,13 +16,8 @@ const hbs = exphbs.create();
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-<<<<<<< HEAD
 app.use(routes);
 app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-=======
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
