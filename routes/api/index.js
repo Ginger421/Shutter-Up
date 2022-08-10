0134e526fdcce6d17cf099ce130724af04f1b922
Ginger Owen <gingerowen0421@gmail.com>
2022-08-09 20:51:04 -0600

    added view all resources, delete resource, but need to figure out file name

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,10 +6,3 @@ const userRoutes = require("./userRoutes");
 router.use("/users", userRoutes);
 
 module.exports = router;
-
-
-POST https://api.cloudinary.com/v1_1/demo/image/upload
-	.then(result => {console.log(result)})
-	.catch(error => {console.log(error)});
-
-router.post
