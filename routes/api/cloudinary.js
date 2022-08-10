66e36f4cb96ae274bccc160f98554ef0e159c308
Ginger Owen <gingerowen0421@gmail.com>
2022-08-09 21:10:39 -0600

    redo of last commit, did not work

--- routes/api/cloudinary.js
+++ routes/api/cloudinary.js
@@ -8,3 +8,15 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.uploader.upload("./images/Planet9_3840x2160.jpg", {resource_type: "auto"})
   .then(result => {console.log(result)})
   .catch(error => {console.log(error)});
+
+
+  // List all assets
+cloudinary.api.resources()
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
+
+
+  // Destroy with Upload API
+cloudinary.uploader.destroy("my_cheesecake", {invalidate: true})
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
--- routes/api/index.js
+++ routes/api/index.js
@@ -8,8 +8,3 @@ router.use("/users", userRoutes);
 module.exports = router;
 
 
-POST https://api.cloudinary.com/v1_1/demo/image/upload
-	.then(result => {console.log(result)})
-	.catch(error => {console.log(error)});
-
-router.post
