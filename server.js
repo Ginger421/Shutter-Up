6d336361fc7dca3b8720c2096cb390cae3900d51
LisaTLe <lisa.le1127@gmail.com>
2022-08-13 14:42:21 -0600

    fix server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const express = require("express");
 const session = require("express-session");
 const controllers = require("./controllers");
 const exphbs = require("express-handlebars");
-const path = require("path");
-const routes = require("./controllers");
+
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const helpers = require("./utils/helpers");
@@ -14,16 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //handlebars here
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(routes);
-app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 
 //authentication w/ cookies & session
 const sess = {
